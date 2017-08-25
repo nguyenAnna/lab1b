1639a858a96ebb21beda4fc148a72d8158da8bcb
Anna Nguyen <thi.nguyen5@dawsoncollege.qc.ca>
2017-08-25 14:08:06 -0400

    Creat and test Address

---
+++ Common/test/lib/AddressTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package lib;
+
+/**
+ * @author 1642202
+ *
+ */
+public class AddressTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Address myAddress = new Address("3040", "Sherbrooke");
+
+		System.out.println(myAddress.getAddress());
+
+	}
+
+}
