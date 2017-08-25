1613f9626d4822bf1d2aff978911f089fe87e55b
Anna Nguyen <thi.nguyen5@dawsoncollege.qc.ca>
2017-08-25 14:10:57 -0400

    Create AddressBook application

---
+++ AddressBookSys/src/addressbook/app/AddressBook.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package addressbook.app;
+import lib.Address;
+
+
+/**
+ * @author 1642202
+ *
+ */
+public class AddressBook {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Address myAddress = new Address("3040", "Sherbrooke");
+		System.out.println(myAddress.getAddress());
+	}
+}
+
